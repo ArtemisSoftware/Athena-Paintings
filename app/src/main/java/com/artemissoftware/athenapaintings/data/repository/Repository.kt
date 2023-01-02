d1df4fb3f10226a953afcbc2751cc526c00d0fad
gustavo <gustavo.isaac@gmail.com>
2023-01-02 10:17:12 +0000

    Repository

---
+++ app/src/main/java/com/artemissoftware/athenapaintings/data/repository/Repository.kt
@@ -0,0 +1,32 @@
+package com.artemissoftware.athenapaintings.data.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.artemissoftware.athenapaintings.data.database.UnsplashDatabase
+import com.artemissoftware.athenapaintings.data.models.UnsplashImage
+import com.artemissoftware.athenapaintings.data.paging.UnsplashRemoteMediator
+import com.artemissoftware.athenapaintings.data.remote.UnsplashApi
+import com.artemissoftware.athenapaintings.util.Constants.ITEMS_PER_PAGE
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@ExperimentalPagingApi
+class Repository @Inject constructor(
+    private val unsplashApi: UnsplashApi,
+    private val unsplashDatabase: UnsplashDatabase
+) {
+
+    fun getAllImages(): Flow<PagingData<UnsplashImage>> {
+        val pagingSourceFactory = { unsplashDatabase.unsplashImageDao().getAllImages() }
+        return Pager(
+            config = PagingConfig(pageSize = ITEMS_PER_PAGE),
+            remoteMediator = UnsplashRemoteMediator(
+                unsplashApi = unsplashApi,
+                unsplashDatabase = unsplashDatabase
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+}
