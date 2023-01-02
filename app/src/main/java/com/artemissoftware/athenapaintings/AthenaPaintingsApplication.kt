78533cb6068eaf1c1b1812ccf54dacdf9ebc00f2
gustavo <gustavo.isaac@gmail.com>
2023-01-02 09:21:42 +0000

    Dagger-Hilt Setup

---
+++ app/src/main/java/com/artemissoftware/athenapaintings/AthenaPaintingsApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.athenapaintings
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AthenaPaintingsApplication : Application()
