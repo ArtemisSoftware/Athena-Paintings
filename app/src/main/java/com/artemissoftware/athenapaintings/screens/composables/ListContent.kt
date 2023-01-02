72bb1d776a8f914061d29fb588bab8394ce2c5a0
gustavo <gustavo.isaac@gmail.com>
2023-01-02 10:30:37 +0000

    Composables

---
+++ app/src/main/java/com/artemissoftware/athenapaintings/screens/composables/ListContent.kt
@@ -0,0 +1,34 @@
+package com.artemissoftware.athenapaintings.screens.composables
+
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.items
+import coil.annotation.ExperimentalCoilApi
+import com.artemissoftware.athenapaintings.data.models.UnsplashImage
+
+@ExperimentalCoilApi
+@Composable
+fun ListContent(items: LazyPagingItems<UnsplashImage>) {
+    Log.d("Error", items.loadState.toString())
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(all = 12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(
+            items = items,
+            key = { unsplashImage ->
+                unsplashImage.id
+            }
+        ) { unsplashImage ->
+            unsplashImage?.let { UnsplashItem(unsplashImage = it) }
+        }
+    }
+}
