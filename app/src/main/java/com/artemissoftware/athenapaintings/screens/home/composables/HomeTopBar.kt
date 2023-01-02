62830c7f4dbbe9585e9b40fc46a02f4a19a7491d
gustavo <gustavo.isaac@gmail.com>
2023-01-02 10:34:21 +0000

    HomeTopBar

---
+++ app/src/main/java/com/artemissoftware/athenapaintings/screens/home/composables/HomeTopBar.kt
@@ -0,0 +1,38 @@
+package com.artemissoftware.athenapaintings.screens.home.composables
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.artemissoftware.athenapaintings.ui.theme.topAppBarBackgroundColor
+import com.artemissoftware.athenapaintings.ui.theme.topAppBarContentColor
+
+@Composable
+fun HomeTopBar(
+    onSearchClicked: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Home",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview
+fun HomeTopBarPreview() {
+    HomeTopBar {}
+}
